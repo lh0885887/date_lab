bae6b1ae1a0ce21cd29b103067f424745def11ce
HILL <lh0885887@dt050585.otc.edu>
2025-09-10 09:16:39 -0500

    Lab complete

--- date_lab.php
+++ date_lab.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 function showNow(): string
 {
     // Sets timestamp for current datetime
@@ -33,15 +31,38 @@ function showFourthOfJuly()
 
 function addThirtyDaysBySeconds()
 {
+    $timestamp = time();
+    $secondsToAdd = (30 * 24 * 60 * 60);
+    $futureTimestamp = strtotime("+ $secondsToAdd seconds", $timestamp);
+    $formatted_timestamp = date("Y-m-d h:i a", $futureTimestamp);
+    return $formatted_timestamp;
+}
+
+function addOneYearRelative()
+{
+    $timestamp = time();
+    $futureTimestamp = strtotime("+ 1 year", $timestamp);
+    $formatted_timestamp = date("Y-m-d h:i", $futureTimestamp);
+    return $formatted_timestamp;
+}
+
+function showCentralTime()
+{
+    $timestamp = time();
+    echo '6. showCentralTime(): ' . "\n" . 'Current time/date: ' . date("Y-m-d h:i a", $timestamp) . "\n";
+    date_default_timezone_set('America/Chicago') . "\n";
+    echo 'Current time/date (CST): ' . date("Y-m-d h:i a", $timestamp);
 
 }
 
 
 
 
-echo '1. showNow(): ' . showNow() . "\n";
-echo "2a. showStringDate('next Friday'): " . showStringDate("next Friday") . "\n";
-echo "2b. showStringDate('2025-12-25'): " . showStringDate("2025-12-25") . "\n";
-echo "3. showFourthOfJuly(): " . showFourthOfJuly() . "\n";
-echo addThirtyDaysBySeconds();
+echo "\n" . '1. showNow(): ' . showNow() . "\n\n";
+echo "2a. showStringDate('next Friday'): " . showStringDate("next Friday") . "\n\n";
+echo "2b. showStringDate('2025-12-25'): " . showStringDate("2025-12-25") . "\n\n";
+echo "3. showFourthOfJuly(): " . showFourthOfJuly() . "\n\n";
+echo "4. addThirtyDaysBySeconds(): " . addThirtyDaysBySeconds() . "\n\n";
+echo "5. addOneYearRelative(): " . addOneYearRelative() . "\n\n";
+echo showCentralTime() . "\n\n";
 ?>
